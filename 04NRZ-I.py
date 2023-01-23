a780a4d59fbe44545bf875efeefe14ae7f9570b7
bayazidHossain2 <bh.190140@s.pust.ac.bd>
2023-01-23 22:57:38 +0600

    Updated

--- 04NRZ-I.py
+++ 04NRZ-I.py
@@ -25,18 +25,37 @@ def getSignal(bits):
     print('Y is :',y_co)
     return [x_co,y_co]
 
-
 #Wev Ploting
 #plt.plot([0,1,1,2,2,3,3,4,5,5,6,7,8,8,9,9,10,10],[1,1,0,0,1,1,0,0,0,1,1,1,1,0,0,1,1,0],color='red')
 while True:
     inp = input('Enter your digital signal :')
-    sig = getSignal(inp)
+    y = list()
+    for ch in inp:
+        if ch is '0':
+            y.append(0)
+        else:
+            y.append(1)
+    y.append(0)
+    x = np.arange(0,len(inp)+1,1)
+    print('y is :',y)
+    figure, axis = plt.subplots(2)
+    # For Unipolar Representation
+    axis[0].grid(True)
+    axis[0].step(x,y)
+    axis[0].set_title("Unipolar Representation of \'"+inp+"\'")
 
-    #Axis Ploting
-    plt.title("NRZ-I Digital to Digital conversion :")
-    plt.xlabel('Input signal is :'+inp)
-    plt.scatter(np.arange(0,len(inp)+1,1),[0]*(len(inp)+1),color='pink')
-    plt.plot([0,len(inp)+2],[0,0],color='blue')
-    plt.plot([0,0],[-1.5,1.5],color='blue')
-    plt.plot(sig[0],sig[1],color='red')
+    sig = getSignal(inp)
+    # For NRZ-I representation
+    axis[1].grid(True)
+    axis[1].plot(sig[0],sig[1])
+    axis[1].set_title("NRZ-I representation of \'"+inp+"\'")
     plt.show()
+
+
+    # #Axis Ploting
+    # plt.title("NRZ-I Digital to Digital conversion :")
+    # plt.xlabel('Input signal is :'+inp)
+    # plt.scatter(np.arange(0,len(inp)+1,1),[0]*(len(inp)+1),color='pink')
+    # plt.plot([0,len(inp)+2],[0,0],color='blue')
+    # plt.plot([0,0],[-1.5,1.5],color='blue')
+    # plt.plot(sig[0],sig[1],color='red')
