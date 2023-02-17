e975f7dea808aa229da1d26b647f35c1ba08e733
bayazidHossain2 <67774055+bayazidHossain2@users.noreply.github.com>
2023-02-18 00:09:29 +0600

    Update 12HammingCode.py

--- Lab Final/12HammingCode.py
+++ Lab Final/12HammingCode.py
@@ -1,17 +1,11 @@
 import numpy as np
-
 inp_signal = input('Enter Message Signal : ')
-
 m = len(inp_signal)
 r = int(np.log2(m))
-
 while(2**r < m+r+1):
     r = r + 1
-
 l = m+r 
-
 print('-> Message len :',m,' -> Redundant len :',r,' -> Total len :',l)
-
 hammingCode = list()
 sig_ind = 0
 for i in range(1,l+1):
@@ -21,7 +15,6 @@ for i in range(1,l+1):
         hammingCode.append(ord(inp_signal[sig_ind])-48)
         sig_ind = sig_ind + 1
 print('Before Calculating Redundant Bit : ',hammingCode)
-
 for i in range(1,l+1):
     if (np.log2(i)-int(np.log2(i)) == 0.0):
         print('\t',i,'th bit parity calculation')
@@ -40,7 +33,6 @@ for i in range(1,l+1):
                 ind = ind + 1
         hammingCode[i-1] = parity%2
         print('\t',i,'th Parity will :',hammingCode[i-1])
-
 print('-----------Generated Hamming code is------------')
 print(hammingCode)
 
